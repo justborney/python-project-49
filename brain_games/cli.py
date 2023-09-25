3ad0925e8c33d4a3afbdccd408912da1745b3319
justborney <justborney@gmail.com>
2023-09-25 15:35:01 +0500

    finished lint

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,9 @@
+"""Brain games cli module."""
 import prompt
 
+
 def welcome_user():
-	print('Welcome to the Brain Games!')
-	name = prompt.string('May I have your name? ')
-	welcome_message = f'Hello, {name}!'
-	print(welcome_message)
+    """Get an user's name and promt that username."""
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,13 @@
 #!/user/bin/env python3
+"""Command line interface for brain games."""
 
 from brain_games.cli import welcome_user
 
 
 def main():
-	welcome_user()
+    """Entry piont for cli."""
+    welcome_user()
 
 
-if __name__ == "__main__":
-	main()
-
+if __name__ == '__main__':
+    main()
